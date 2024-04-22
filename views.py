497cc6e69ebbe0c1e9879e34d74d1ff4c3abdb13
angelopchery <141708179+angelopchery@users.noreply.github.com>
2024-04-22 13:33:16 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from .models import TodoListItem 
+from django.http import HttpResponseRedirect 
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+
+def todoappView(request):
+    all_todo_items = TodoListItem.objects.all()
+    return render(request, 'todolist.html',  {'all_items':all_todo_items})
+
+
+def addTodoView(request):
+    new_item = TodoListItem()
+    new_item.content = request.POST.get('content')
+    new_item.save()
+    return HttpResponseRedirect('/todo/') 
+
+
+def deleteTodoView(request, id):
+    y = TodoListItem.objects.get(id= id)
+    y.delete()
+    return HttpResponseRedirect('/todo/') 
+
+def updateTodo(request, id):
+    todo_item = TodoListItem.objects.get(id=id)
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        todo_item.content = content
+        todo_item.save()
+        return HttpResponseRedirect('/todo/')
+    return render(request, 'update.html', {'todo_item': todo_item})
+
+
+
+    
+
+
+
